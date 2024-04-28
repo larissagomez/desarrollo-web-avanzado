64c734e6476a3063609d0ab3ca9973ab260437fb
larissagomez <gomezmartinezlarissa68@gmail.com>
2024-04-28 16:02:29 -0600

    Muestra de imagenes y estilos

--- 04-rick-and-morty/src/App.jsx
+++ 04-rick-and-morty/src/App.jsx
@@ -4,27 +4,48 @@ import './App.css'
 
 function App() {
   const [data, setData] = useState({results: []})
+  const [query, setQuery] = useState('')
+  const [search, setSearch] = useState ('')
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    setSearch(query)
+  }
 
   const searchCharacter = async () => {
-    const result = await axios ('https://rickandmortyapi.com/api/character')
+    const result = await axios (`https://rickandmortyapi.com/api/character/?name=${query}`)
 
     setData(result.data)
   }
 
   useEffect(() => {
     searchCharacter()
-  }, [])
+  }, [search],)
 
   return (
     <>
       <h1>Rick and Morty</h1>
-      <div className='results'>
-        <ul>
-          {data.results.map(item => (
-            <li key={item.id}> {item.name} </li>
-          ))}
-        </ul>
+
+      <div className="results-wrapper">
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={query} onChange={e => setQuery(e.target.value)} />
+          <button type="search-button"> Buscar </button>
+        </form>
+        <div>
+          <ul className='grid-cards'>
+            {data.results.map(item => (
+              <li className='image-container' key={item.id}> 
+                <img src={item.image} alt={item.name}/>
+                <h2 className='name'>{item.name}</h2>
+                <span className='description'>Estatus: {item.status}</span>
+                <span className='description'>Especie: {item.species}</span>
+                <span className='description'>Genero: {item.gender}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
+      
     </>
   )
 }
